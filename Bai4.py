9a12f451b878156df65d7a21000b3842240d56c0
nguyenxuanson01012007-ship-it <nguyenxuanson01012007@gmail.com>
2026-05-19 23:08:26 +0700

    Update employee input handling in Bai4.py
    Refactor employee input logic to ensure valid count.

--- Bai4.py
+++ Bai4.py
@@ -1,11 +1,15 @@
-print('-- HỆ THỐNG KHAI BÁO NHÂN SỰ MỚI --')
-while True:
-    try:
-        new_emp = int(input('Nhập số lượng nhân viên mới:'))
-        if new_emp > 0:
-            print(f'Ghi nhận thành công {new_emp} nhân viên mới')
-            break;
-        else :
-            print('Số lượng không hợp lệ')
-    except ValueError:
-        print(" Lỗi: Vui lòng nhập một số nguyên hợp lệ.")
+new_employee_count = 0
+
+while new_employee_count <= 0:
+
+    new_employee_count = int(
+        input("Vui lòng nhập số lượng nhân sự mới trong tháng này: ")
+    )
+
+    if new_employee_count <= 0:
+
+        print("❌ LỖI: Số lượng nhân sự phải lớn hơn 0!")
+
+
+print(" Ghi nhận thành công.")
+print("Số lượng nhân sự mới là:", new_employee_count)
